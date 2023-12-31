e7e6dc3f16207b49b405dd8f2dc7da4db0c29b65
DamCuvelier <71203990+damcuvelier@users.noreply.github.com>
2023-12-31 18:33:22 +0100

    Add files via upload

---
+++ Install-GitHubModule.psm1
@@ -0,0 +1,20 @@
+function Install-GitHubModule{
+param($Name,$Repo)
+$erroractionpreference = 'silentlycontinue'
+Set-ExecutionPolicy -ExecutionPolicy bypass -force
+$here = $PSScriptRoot; if(!$here){$here = (Get-Location).path}
+
+if(!(test-path "$here\$Name.psm1" -ErrorAction silentlycontinue)){
+Install-Module -Name InstallModuleFromGitHub -RequiredVersion 0.3
+Install-ModuleFromGitHub -GitHubRepo $Repo -Branch $Name
+if(test-path "$here\$Name.zip" -ErrorAction silentlycontinue){Remove-Item "$here\$Name.zip" -Force -Confirm:$false | out-null}
+if(test-path "$here\CMScripts-$Name" -ErrorAction silentlycontinue){
+	Copy-Item "$here\CMScripts-$Name\$Name.psm1" -Destination $here -force
+	Remove-Item "$here\CMScripts-$Name" -Recurse -Force -Confirm:$false | out-null
+}
+}
+import-module "$here\$Name.psm1" -DisableNameChecking -Force | out-null
+
+
+}
+
